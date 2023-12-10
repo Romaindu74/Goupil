5b77b2d9e61d574b447c9d654528dc8c41baf24d
Romaindu74 <91608155+Romaindu74@users.noreply.github.com>
2023-12-10 15:44:43 +0100

    Update main.py

--- src/Logs/main.py
+++ src/Logs/main.py
@@ -5,40 +5,40 @@ class Colors:
     def __init__(self) -> None:
         self.enable = True if (os.name == 'posix' and 'TERM' in os.environ and os.environ['TERM'] != 'dumb') or (os.name == 'nt' and os.environ.get('TERM_PROGRAM') == 'vscode') else False
 
-    def init(self, x, y, txt):
+    def init(self, x: int, y: int, txt: str):
         rgx = '\x1b[{}m'.format(y)
         open_code = '\x1b[{}m'.format(x)
         close_code = '\x1b[{}m'.format(y)
         return open_code + (txt.replace(rgx, close_code + open_code) if txt and rgx in txt else txt) + close_code if txt else txt
 
-    def black(self, txt):
+    def black(self, txt: str):
         return self.init(30, 39, txt)
 
-    def red(self, txt):
+    def red(self, txt: str):
         return self.init(31, 39, txt)
 
-    def green(self, txt):
+    def green(self, txt: str):
         return self.init(32, 39, txt)
 
-    def yellow(self, txt):
+    def yellow(self, txt: str):
         return self.init(33, 39, txt)
 
-    def blue(self, txt):
+    def blue(self, txt: str):
         return self.init(34, 39, txt)
 
-    def magenta(self, txt):
+    def magenta(self, txt: str):
         return self.init(35, 39, txt)
 
-    def cyan(self, txt):
+    def cyan(self, txt: str):
         return self.init(36, 39, txt)
 
-    def white(self, txt):
+    def white(self, txt: str):
         return self.init(37, 39, txt)
 
-    def gray(self, txt):
+    def gray(self, txt: str):
         return self.init(90, 39, txt)
 
-    def grey(self, txt):
+    def grey(self, txt: str):
         return self.init(90, 39, txt)
 
 class Logs:
@@ -46,7 +46,7 @@ class Logs:
         self.path = os.getcwd()
         self.colors = Colors()
 
-    def setup(self, config):
+    def setup(self, config: dict):
         if isinstance(config.get('pathfile'), str):
             pathfile = os.path.join(self.path, config['pathfile'])
             if not os.path.exists(pathfile):
@@ -59,7 +59,7 @@ class Logs:
         if isinstance(config.get('prefixlog'), str):
             self.pl = config['prefixlog']
 
-    def gt(self, l):
+    def gt(self, l: int):
         return {
             5: {'type': 'CRITICAL', 'color': self.colors.red},
             4: {'type': 'ERROR', 'color': self.colors.red},
@@ -68,14 +68,14 @@ class Logs:
             1: {'type': 'DEBUG', 'color': self.colors.white}
         }.get(l)
 
-    def dt(self, t):
+    def dt(self, t: str):
         formatted_parts = datetime.now().strftime('%A %Y %m %d %H %M').split()
         token_replacements = [formatted_parts[4], formatted_parts[5], formatted_parts[2], formatted_parts[3], formatted_parts[1]]
         for token, replacement in zip(["%H", "%M", "%j", "%m", "%a"], token_replacements):
             t = t.replace(token, replacement)
         return t
 
-    def s(self, t):
+    def s(self, t: str):
         file = self.path
         if hasattr(self, 'pf'):
             file = os.path.join(file, self.pf)
@@ -87,7 +87,7 @@ class Logs:
         with open(file, 'a', encoding='utf-8') as f:
             f.write(t + '\n')
 
-    def p(self, l, t):
+    def p(self, l: int, t: str):
         level = self.gt(l)
         if level:
             txt = self.pl and self.dt("{}{}".format(self.pl, t)) or t
@@ -95,18 +95,17 @@ class Logs:
                 self.s(txt.replace("%type", level['type']))
             print(txt.replace('%type', level['color'](level['type'])))
 
-    def debug(self, *text):
+    def debug(self, *text: str):
         self.p(1, ' '.join(text))
 
-    def info(self, *text):
+    def info(self, *text: str):
         self.p(2, ' '.join(text))
 
-    def warning(self, *text):
+    def warning(self, *text: str):
         self.p(3, ' '.join(text))
 
-    def error(self, *text):
+    def error(self, *text: str):
         self.p(4, ' '.join(text))
 
-    def critical(self, *text):
+    def critical(self, *text: str):
         self.p(5, ' '.join(text))
-
